9934235599f2b65a613459f79d8a37facd21f85e
Michael Herman <hermanmu@gmail.com>
2015-08-28 18:10:01 -0600

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -3,38 +3,37 @@ var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 
 if (computerChoice <= 0.33) {
-    computerChoice = "rock";
+  computerChoice = "rock";
 } else if (computerChoice <= 0.66) {
-    computerChoice = "paper";
+  computerChoice = "paper";
 } else {
-    computerChoice = "scissors";
+  computerChoice = "scissors";
 }
 
 var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-    return "The result is a tie!";
-} else if(choice1 ==="rock") {
+  if (choice1 === choice2) {
+    return "?";
+  } else if(choice1 ==="rock") {
     if (choice2==="scissors") {
-    return "rock wins";
-} else {
-    return "paper wins";
-        }
+      return "?";
+    } else {
+      return "?";
     }
-   else if(choice1==="paper") {
-       if(choice2 ==="rock") {
-           return "paper wins";
-       } else {
-           return "scissors wins";
-       }
-   }
-
-    else if(choice1==="scissors") {
-        if (choice2==="rock") {
-            return "rock wins";
-        } else {
-            return "scissors wins";
-        }
+  }
+  else if(choice1==="paper") {
+    if(choice2 ==="rock") {
+      return "?";
+    } else {
+      return "?";
     }
+  }
+  else if(choice1==="scissors") {
+    if (choice2==="rock") {
+      return "?";
+    } else {
+      return "?";
+    }
+  }
 
 };
 
